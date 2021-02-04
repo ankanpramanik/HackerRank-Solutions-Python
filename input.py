9c7db0b1b8da621a856074581e37cd676eb8c864
Ankan Pramanik <72189396+ankanpramanik@users.noreply.github.com>
2021-02-04 23:09:26 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+x,k=(input().split())
+x=int(x)
+k=int(k)
+pol=input()
+ans=(eval(pol))
+if ans==k:
+    print("True")
+else: print("False")
---
+++ swap_case.py
@@ -0,0 +1,5 @@
+def swap_case(s):
+    x=s.swapcase()
+
+    return x
+
